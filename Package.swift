5e10d6442e45ba249eb4041e9824df0ecf3d9e45
Rudrank Riyam <rudrankriyam@gmail.com>
2025-01-16 15:02:17 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -20,8 +20,8 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/huggingface/swift-transformers.git", exact: "0.1.8"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", exact: "1.3.0"),
+        .package(url: "https://github.com/huggingface/swift-transformers", from: "0.1.13"),
     ],
     targets: [
         .target(
